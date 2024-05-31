b2e53a38a1acd7cab021ee58f50adc1cb37ba73a
Shakillazycoder <sakil.khan882.sk@gmail.com>
2024-05-31 23:13:20 +0600

    Menu cerate and page setup done

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,11 +2,35 @@ import {
     createBrowserRouter,
   } from "react-router-dom";
 import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home";
+import Doctors from "../Pages/Doctors/Doctors";
+import Appointment from "../Pages/Appointment/Appointment";
+import About from "../Pages/About/About";
   
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Main/>,
+      errorElement: <p>Error</p>,
+      children: [
+        {
+            path: '/',
+            element: <Home/>
+        },
+        {
+            path: '/doctors',
+            element: <Doctors/>
+        },
+        {
+            path: '/appointment',
+            element: <Appointment/>
+        },
+        {
+            path: '/about',
+            element: <About/>
+        },
+
+      ]
     },
   ]);
 
--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -55,6 +55,7 @@ const Navbar = () => {
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 gap-2 md:bg-white  dark:border-gray-700">
               <li>
                 <NavLink
+                to='/'
                   className="block py-2 px-3 md:p-0  bg-blue-700 rounded md:bg-transparent text-gray-900 md:dark:text-blue-500"
                   aria-current="page"
                 >
@@ -63,26 +64,26 @@ const Navbar = () => {
               </li>
               <li>
                 <NavLink
-                  href="#"
+                 to='/doctors'
                   className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark: dark:hover:bg-blue-700 dark:hover: md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  About
+                  Doctors
                 </NavLink>
               </li>
               <li>
                 <NavLink
-                  href="#"
+                  to='/appointment'
                   className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark: dark:hover:bg-gray-700 dark:hover: md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Services
+                  Appointment
                 </NavLink>
               </li>
               <li>
                 <NavLink
-                  href="#"
+                  to='/about'
                   className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark: dark:hover:bg-gray-700 dark:hover: md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Contact
+                  About Us
                 </NavLink>
               </li>
             </ul>
