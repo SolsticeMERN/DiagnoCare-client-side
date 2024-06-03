70b02bfee66abd28f0c798ee0d6eb814584555f4
Shakillazycoder <sakil.khan882.sk@gmail.com>
2024-06-03 13:19:35 +0600

    booking test delete function complete

--- src/Pages/Dashboard/User/Appointments/Bookings.jsx
+++ src/Pages/Dashboard/User/Appointments/Bookings.jsx
@@ -1,4 +1,47 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { MdDeleteForever } from "react-icons/md";
+import toast from "react-hot-toast";
+
 const Bookings = () => {
+  const axiosSecure = useAxiosSecure();
+
+  const { data: bookings = [], refetch } = useQuery({
+    queryKey: ["booking"],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get("/booking");
+      return data;
+    },
+  });
+
+  const {mutateAsync} = useMutation({
+    mutationFn: async (_id) => {
+      const {data} = await axiosSecure.delete(`/booking-room/${_id}`)
+      return data
+    },
+    onSuccess: async (data) => {
+      console.log(data);
+      toast.success('Booking cancel successfully')
+
+
+      refetch()
+    },
+    onError: (err) => {
+      console.log(err.message);
+    }
+  })
+
+
+  const handleDelete = async (_id) => {
+    console.log('delete', _id);
+    try{
+     await mutateAsync(_id)
+    }catch (err) {
+      console.log(err);
+    }
+
+  }
+
   return (
     <div>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
@@ -6,16 +49,19 @@ const Bookings = () => {
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Product name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Color
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Category
+                Test name
               </th>
               <th scope="col" className="px-6 py-3">
                 Price
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Date
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Time
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Status
               </th>
               <th scope="col" className="px-6 py-3">
                 Action
@@ -23,7 +69,74 @@ const Bookings = () => {
             </tr>
           </thead>
           <tbody>
-            
+            {bookings.map((booking, index) => {
+              return (
+                <tr
+                  key={index}
+                  className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
+                >
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {booking.title}
+                  </th>
+                  <td className="px-6 py-4">${booking.amountPaid}</td>
+                  <td className="px-6 py-4">
+                    {booking.date
+                      ? new Date(booking.date).toLocaleDateString("en-US", {
+                          year: "numeric",
+                          month: "long",
+                          day: "numeric",
+                        })
+                      : "N/A"}
+                  </td>
+                  <td className="px-6 py-4">{booking.time} PM</td>
+                  <td className="px-6 py-4">
+                    {" "}
+                    <div
+                      className={`inline-flex items-center px-3 py-1 rounded-full gap-x-2 ${
+                        booking.reportStatus === "pending" &&
+                        "bg-yellow-100/30 text-yellow-500"
+                      } ${
+                        booking.reportStatus === "In Progress" &&
+                        "bg-blue-100/30 text-blue-500"
+                      } ${
+                        booking.reportStatus === "Complete" &&
+                        "bg-emerald-100/30 text-emerald-500"
+                      } ${
+                        booking.reportStatus === "Rejected" &&
+                        "bg-red-100/30 text-red-500"
+                      } `}
+                    >
+                      <span
+                        className={`h-1.5 w-1.5 rounded-full ${
+                          booking.reportStatus === "pending" && "bg-yellow-500"
+                        } ${
+                          booking.reportStatus === "In Progress" &&
+                          "bg-blue-500"
+                        } ${
+                          booking.reportStatus === "Complete" && "bg-green-500"
+                        } ${
+                          booking.reportStatus === "Rejected" && "bg-red-500"
+                        }  `}
+                      ></span>
+                      <h2 className="text-sm font-normal ">
+                        {booking.reportStatus}
+                      </h2>
+                    </div>
+                  </td>
+                  <td className="px-6 py-4">
+                    <button
+                      onClick={() => handleDelete(booking._id)}
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      <MdDeleteForever size={30} className="text-red-500" />
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
